305b74a8aa59e46dfd3558c15a62301a8bde3a19
Michał Śmiarowski <indfear@gmail.com>
2017-08-21 23:46:42 +0100

    Registration and Login - alpha version

---
+++ WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginController : SurfaceController
+    {
+        // GET: Login
+        public ActionResult Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+                return CurrentUmbracoPage();
+
+            if(Members.Login(model.Username, model.Password))
+            {
+                return Redirect("/");
+            }
+
+            ModelState.AddModelError("", "Invalid Login");
+
+            return CurrentUmbracoPage();
+        }
+
+        public ActionResult Logout()
+        {
+            Members.Logout();
+            return Redirect("/");
+        }
+    }
+}
---
+++ WebApplication1/Controllers/RegisterController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class RegisterController : SurfaceController
+    {
+        // GET: Register
+        public ActionResult Register(RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+                return CurrentUmbracoPage();
+
+            var memberService = Services.MemberService;
+
+            if (memberService.GetByEmail(model.Email) != null)
+            {
+                ModelState.AddModelError("", "A member with that email already exists");
+                return CurrentUmbracoPage();
+            }
+
+            var member = memberService.CreateMemberWithIdentity(model.Email, model.Email, model.Name, "siteMember");
+
+            member.SetValue("bio", model.Biography);
+            member.SetValue("avatar", model.Avatar);
+            memberService.SavePassword(member, model.Password);
+            Members.Login(model.Email, model.Password);
+
+            memberService.Save(member);
+
+            return Redirect("/");
+        }
+    }
+}
